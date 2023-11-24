5114312793f2cf48aae114a95753a58511380d26
rovannoufer <noufermohamed007@gmail.com>
2023-11-25 00:21:10 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import React from 'react';
+// import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import { TodoWrapper } from './components/TodoWrapper';
 
 function App() {
   return (
-    <div className="App">
-      <TodoWrapper />
-    </div>
+    
+       <div>
+          <TodoWrapper />
+      </div>
   );
 }
 
